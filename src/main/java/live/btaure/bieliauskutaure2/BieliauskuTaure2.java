26d82bf394b624acb8488ed3a2872c48ffd53d29
salvijusbieliauskas <salvijusbagdzevicius@gmail.com>
2022-11-28 21:08:03 +0200

    Initial commit

---
+++ src/main/java/live/btaure/bieliauskutaure2/BieliauskuTaure2.java
@@ -0,0 +1,13 @@
+package live.btaure.bieliauskutaure2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BieliauskuTaure2 extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("prasideda.");
+    }
+    @Override
+    public void onDisable() {
+    }
+}
