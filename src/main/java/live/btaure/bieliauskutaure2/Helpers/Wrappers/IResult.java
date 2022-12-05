738da3041c6a09ed0bbdfd9ebdbc78aa36640c40
Laurynas <laurynas.cekavicius@ktu.edu>
2022-12-05 20:30:21 +0200

    Added some basic, unfinished infrastructure

---
+++ src/main/java/live/btaure/bieliauskutaure2/Helpers/Wrappers/IResult.java
@@ -0,0 +1,8 @@
+package live.btaure.bieliauskutaure2.Helpers.Wrappers;
+
+public interface IResult
+{
+    boolean isSuccessful = false;
+    public boolean isSuccessful();
+    public void setSuccessful(boolean successful);
+}
---
+++ src/main/java/live/btaure/bieliauskutaure2/Helpers/Wrappers/Result.java
@@ -0,0 +1,23 @@
+package live.btaure.bieliauskutaure2.Helpers.Wrappers;
+
+public class Result<T> implements IResult
+{
+    private T result;
+    private boolean isSuccessful;
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public void setSuccessful(boolean successful) {
+        isSuccessful = successful;
+    }
+}
