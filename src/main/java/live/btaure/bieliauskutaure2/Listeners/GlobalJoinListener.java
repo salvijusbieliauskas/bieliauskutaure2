1e66b183cf6c4745545c4cc4ffd2eac9688d9d98
salvijusbieliauskas <salvijusbagdzevicius@gmail.com>
2022-12-06 20:44:49 +0200

    Fixed scoreboard updating on player join

--- src/main/java/live/btaure/bieliauskutaure2/Listeners/GlobalJoinListener.java
+++ src/main/java/live/btaure/bieliauskutaure2/Listeners/GlobalJoinListener.java
@@ -6,14 +6,15 @@ import live.btaure.bieliauskutaure2.Participants.PlayerManager;
 import live.btaure.bieliauskutaure2.Participants.Spectator;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public class GlobalJoinListener implements Listener {
     @EventHandler
-    public void onPlayerJoin(PlayerLoginEvent event)
+    public void onPlayerJoin(PlayerJoinEvent event)
     {
         PlayerManager.getInstance().addBTPlayer(new Spectator(event.getPlayer().getUniqueId(),null), AddModeType.CHECK);
         BTPlayer player = PlayerManager.getInstance().getBTPlayer(event.getPlayer().getUniqueId());
-        player.updateScoreboard();//TODO:sitas neveikia nes zaidejai nespeja isiloadint
+        player.updateScoreboard();
     }
 }
